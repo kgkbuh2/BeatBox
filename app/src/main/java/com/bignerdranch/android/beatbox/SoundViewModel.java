ee3d3ccd2a0a873d37285b1d9d489cdc3c7345b3
tatianatsygankova <uliana1996>
2018-08-05 23:31:54 +0300

    Тест

--- app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
@@ -29,4 +29,8 @@ public class SoundViewModel extends BaseObservable {
         mSound = sound;
         notifyChange();
     }
+
+    public void onButtonClicked() {
+        mBeatBox.play(mSound);
+    }
 }
--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
@@ -1,9 +1,13 @@
 package com.bignerdranch.android.beatbox;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by cygankovatatana on 02.08.2018.
@@ -20,6 +24,18 @@ public class SoundViewModelTest {
         mSound = new Sound("assetPath");
         mSubject = new SoundViewModel(mBeatBox);
         mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exposesSoundNameAsTitle() {
+        assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+
+    @Test
+    public void callsBeatBoxPlayOnButtonClicked() {
+        mSubject.onButtonClicked();
+
+        verify(mBeatBox).play(mSound);
     }
 
 }
